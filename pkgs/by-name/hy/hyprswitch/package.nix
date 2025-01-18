fc3048b8d46cc7481493b31ab9fe5535b56f2449
WrkX <32910499+WrkX@users.noreply.github.com>
2025-01-18 11:51:25 +0100

    Add files via upload

---
+++ pkgs/by-name/hy/hyprswitch/package.nix
@@ -0,0 +1,28 @@
+{ lib, pkgs, pkg-config, rustPlatform, makeWrapper, fetchFromGitHub }: let
+  pdescription = "Window Switcher for Hyprland";
+  pversion = "v3.2.0";
+in
+  rustPlatform.buildRustPackage rec {
+    name = "hyprswitch";
+    version = pversion;
+
+    src  = fetchFromGitHub {
+       owner = "WrkX";
+       repo = "hyprswitch";
+       rev = "v3.2.0";
+       hash = "sha256-Zc42gp9ZoYE3/yWAQtBa+cBSjaam/e3OffJkrQTUvWc=";
+     };
+
+    cargoLock.lockFile = ./Cargo.lock;
+
+    nativeBuildInputs = [ pkg-config makeWrapper ];
+    buildInputs = with pkgs; [ glib gtk4 gtk4-layer-shell ];
+
+    postInstall = ''
+      wrapProgram $out/bin/${name}
+    '';
+
+    meta = {
+      description = pdescription;
+    };
+  }
