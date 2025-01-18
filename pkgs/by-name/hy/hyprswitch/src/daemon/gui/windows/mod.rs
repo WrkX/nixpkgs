1f16a932d0a24838be409fbd3d7dece426877592
WrkX <32910499+WrkX@users.noreply.github.com>
2025-01-18 10:25:46 +0100

    Delete pkgs/by-name/hy/hyprswitch directory

--- pkgs/by-name/hy/hyprswitch/src/daemon/gui/windows/mod.rs
+++
@@ -1,9 +0,0 @@
-mod create;
-mod init;
-mod update;
-pub mod click;
-mod switch_fns;
-
-pub use create::create_windows;
-pub use init::init_windows;
-pub use update::update_windows;
--- pkgs/by-name/hy/hyprswitch/src/handle/sort/mod.rs
+++
@@ -1,8 +0,0 @@
-pub use sort::sort_clients;
-pub use update::update_clients;
-
-#[allow(clippy::module_inception)]
-mod sort;
-#[cfg(test)]
-mod tests;
-mod update;
